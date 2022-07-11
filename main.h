891e43dd17474e3174ea42b5d67613eac9ca7b84
Felloh31 <felixodhiambo31@live.com>
2022-07-11 18:54:48 +0300

    +header

--- main.h
+++ main.h
@@ -5,5 +5,22 @@
 #include <stdlib.h>
 
 int _printf(const char *format, ...);
+int print_char(va_list c);
+int print_string(va_list s);
+int print_string(va_list s);
+int print_int(va_list i);
+
+
+/**
+  * struct code_format - Struct format
+  * @sc: The specifiers
+  * @f: The function associated
+  */
+
+typedef struct code_format
+{
+	char *sc;
+	int (*f)(va_list);
+} code_f;
 
 #endif
