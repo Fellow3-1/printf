120ea8b1d829e05fe5a4f02c2cb8533566509db6
Felloh31 <felixodhiambo31@live.com>
2022-07-13 01:21:39 +0300

    +C is not fun

--- main.h
+++ main.h
@@ -1,28 +1,14 @@
 #ifndef MAIN_H
 #define MAIN_H
+#define NULL ((void *)0)
 
-#include <stdio.h>
-#include <stdarg.h>
-#include <stdlib.h>
-
-
+int _putchar(char c);
+int _print_str(char *string);
+int _print_int(long int var);
 int _printf(const char *format, ...);
-int print_char(va_list c);
-int print_string(va_list s);
-int print_string(va_list s);
-int print_int(va_list i);
-int (*format_conversion(char s))(va_list);
-
-/**
-  * struct code_format - Struct format
-  * @sc: The specifiers
-  * @f: The function associated
-  */
-
-typedef struct code_format
-{
-	char *sc;
-	int (*f)(va_list);
-} code_f;
+int to_Binary(unsigned int n);
+int to_Octal(unsigned int n);
+int _power_recursion(int x, int y);
+int to_Hexa(unsigned int num);
 
 #endif
