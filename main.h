2fe37659918b9586c68ee51dc3b0dbc707689359
NomatheLuckyOne <thamiemathebula@gmail.com>
2022-07-13 08:04:48 -0700

    another+

--- main.h
+++ main.h
@@ -118,19 +118,3 @@ char *get_precision(char *p, params_t *params, va_list ap);
 int _printf(const char *format, ...);
 
 #endif
-
-
-
-
-
-
-int _putchar(char c);
-int _print_str(char *string);
-int _print_int(long int var);
-int _printf(const char *format, ...);
-int to_Binary(unsigned int n);
-int to_Octal(unsigned int n);
-int _power_recursion(int x, int y);
-int to_Hexa(unsigned int num);
-
-#endif
