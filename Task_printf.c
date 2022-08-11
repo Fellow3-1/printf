75153083405e1d13adfbde6eea74166e7d4a91f7
Felloh31 <felixodhiambo31@live.com>
2022-08-11 19:51:41 +0300

    +Print_F

---
+++ Task_printf.c
@@ -0,0 +1,44 @@
+#include <stdarg.h>
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * @brief 
+ * _printf - prints a string in a formated way
+ * @param formart: string to print (char *) 
+ * @param ... 
+ * @return number of characters printed 
+ */
+
+int _printf(const char *format, ...)
+{
+	va_list valist;
+	unsigned int i = 0;
+	unsigned int result = 0;
+
+	if (format == NULL)
+	{
+		return (-1);
+	}
+	va_start(valist, format);
+
+	for (i = 0; format[i] != '\0'; i++)
+	{
+		if (format[i] == '\0' || (format[i] == '%' && !format[i + 1]))
+		{
+			return (-1);
+		}
+		else if (format[i] == '%' && (format[i + 1] == 'd' || format[i + 1] == 'i' ||
+					format[i + 1] == 's' || format[i + 1] == 'c' || format[i + 1] == '%'))
+		{
+			result += (*format_conversion(format[i + 1]))(valist);
+			i++;
+		}
+		else
+		{
+			result += _putchar(format[i]);
+		}
+	}
+	va_end(valist);
+	return (result);
+}
